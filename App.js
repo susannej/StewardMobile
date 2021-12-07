eda7921d08f8aa1becad94597564af4f12d734d2
Susanne Jaeckel <susanne_j@gmx.de>
2021-12-07 11:42:42 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,87 @@
+import { blockStatement } from '@babel/types';
+import React from 'react';
+import { Alert, Dimensions, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Button, Input } from 'react-native-elements';
+//import { Input } from 'react-native-elements/dist/input/Input';
+import styles from './styles';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const BG_IMAGE = require('./img/bg.jpeg');
+const LUCKE_LOGO = require('./img/cropped-logo-lucke-white.png');
+
+const StewardMobileApp = () => {
+
+	return (
+		<View style={styles.container} >
+			<ImageBackground source={BG_IMAGE} style={styles.bgImage}>
+					<View style={StyleSheet.logoContainer}>
+						<Image source={LUCKE_LOGO} style={styles.luckeLogo} />
+					</View>
+					<View style={styles.titleContainer}> 
+						<View>
+							<Text style={styles.titleText}>Steward Mobile</Text> 
+						</View>
+					</View>
+					<View style={styles.formContainer} >
+						<Input 
+							//value={username}
+							autoFocus={true}
+							autoCapitalize="none"
+							autoCorrect={false}
+							keyboardType="email-address"
+							returnKeyType="next"
+							inputStyle={{ marginLeft: 10, color: 'grey' }}
+							placeholder={'Benutzername'}
+							containerStyle={{
+							  borderBottomColor: 'rgba(0, 0, 0, 0.38)',
+							}}
+							//ref={emailInput}
+							//onSubmitEditing={() => passwordInput.current.focus()}
+							//onChangeText={(text) => setEmail(text)}
+							//errorMessage={
+							  //isEmailValid ? '' : 'Please enter a valid email address'
+							//}
+			  			/>
+						<Input 
+							//value={username}
+							autoFocus={false}
+							autoCapitalize="none"
+							autoCorrect={false}
+							keyboardType="email-address"
+							returnKeyType="next"
+							inputStyle={{ marginLeft: 10, color: 'grey' }}
+							placeholder={'Passwort'}
+							containerStyle={{
+							  borderBottomColor: 'rgba(0, 0, 0, 0.38)',
+							}}
+							//ref={emailInput}
+							//onSubmitEditing={() => passwordInput.current.focus()}
+							//onChangeText={(text) => setEmail(text)}
+							//errorMessage={
+							  //isEmailValid ? '' : 'Please enter a valid email address'
+							//}
+			  			/>
+           				<Button
+              				buttonStyle={styles.loginButton}
+              				containerStyle={{ marginTop: 32, flex: 0 }}
+              				activeOpacity={0.8}
+              				title='Anmelden'
+              				//onPress={isLoginPage ? login : signUp}
+              				titleStyle={styles.loginTextButton}
+              				//loading={isLoading}
+              				//disabled={isLoading}
+            			/>
+            			<Button
+              				buttonStyle={styles.backendButton}
+              				containerStyle={{ marginTop: 32, flex: 0 }}
+              				title={'Konfiguration ...'}
+              				titleStyle={{ color: 'rgba(0, 0, 0, 0.38)' }}
+              				onPress={() => Alert.alert('🤔', 'https://10.101.144.20')}
+            			/>
+          			</View>
+			</ImageBackground>
+		</View>
+	);
+}
+export default StewardMobileApp;
---
+++ src/screens/login/styles.js
@@ -0,0 +1,94 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+	container: {
+		flexGrow: 1,
+		paddingBottom: 20,
+		width: '100%',
+		//height: SCREEN_HEIGHT,
+		height: '100%',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+	luckeLogo: {
+        //flex: 1,
+		//height: 90,
+		//maxWidth: (SCREEN_WIDTH / 4),
+		resizeMode: 'center'
+	},
+	bgImage: {
+	  flex: 1,
+	  top: 0,
+	  left: 0,
+	  width: '100%',
+	  height: SCREEN_HEIGHT,
+	  justifyContent: 'center',
+	  alignItems: 'center',
+    },
+	logoContainer: {
+        //flex: 1,
+	  //flex: 2,
+	  //height: 150,
+	  backgroundColor: 'transparent',
+	  justifyContent: 'center',
+	},
+	titleContainer: {
+        //flex: 1,
+	  //flex: 2,
+	  //height: 150,
+	  backgroundColor: 'transparent',
+	  justifyContent: 'center',
+      paddingTop: 10,
+      paddingBottom: 30,
+	},
+	formContainer: {
+        //flex: 2,
+	  backgroundColor: 'white',
+	  width: SCREEN_WIDTH - 30,
+      maxWidth: 400,
+	  borderRadius: 10,
+	  paddingTop: 32,
+	  paddingBottom: 32,
+	  alignItems: 'center',
+	},
+	loginText: {
+	  fontSize: 16,
+	  fontWeight: 'bold',
+	  color: 'white',
+    },
+	titleText: {
+	  color: 'white',
+	  fontSize: 30,
+	  //fontFamily: 'regular',
+	  textAlign: 'center',
+	},
+	loginTextButton: {
+	  fontSize: 16,
+	  color: 'white',
+	  fontWeight: 'bold',
+	},
+	loginButton: {
+	  backgroundColor: 'rgba(232, 147, 142, 1)',
+	  borderRadius: 10,
+	  height: 50,
+	  width: 200,
+    },
+	backendButton: {
+	  backgroundColor: 'transparent',
+      borderColor: 'rgba(232, 147, 142, 1)',
+	  borderRadius: 10,
+      borderWidth: 3,
+	  height: 50,
+	  width: 200,
+    },
+    helpContainer: {
+        height: 64,
+        alignItems: 'center',
+        justifyContent: 'center',
+  	}
+});
+
+export default styles;
